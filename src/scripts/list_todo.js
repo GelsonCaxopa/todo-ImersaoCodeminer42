f6dea89af94f09de4eb64ee31836c73c756db05b
Gelson Souza <gelson.majorit@gmail.com>
2021-10-05 07:02:32 -0300

    commit

--- src/scripts/list_todo.js
+++ src/scripts/list_todo.js
@@ -1,4 +1,10 @@
 const todoItemsList = document.querySelector('.todo-list');
+const descriptione = document.querySelector('.descriptione');
+const deadline = document.querySelector('.deadlinee');
+const tag = document.querySelector('.tagse');
+const modal = document.querySelector('.modal-container');
+
+
 
 function renderTodos(todos) {
     todoItemsList.innerHTML = '';
@@ -8,7 +14,7 @@ function renderTodos(todos) {
   
       const div = document.createElement('div');
       div.setAttribute('id', 'todoCard')
-      div.setAttribute('class', 'item');
+      div.setAttribute('class', 'item container');
       div.setAttribute('data-key', item.id);
 
       if (item.completed === true) {
@@ -17,17 +23,19 @@ function renderTodos(todos) {
       }else{
           item.status = 'Incompleto';
       }
+
+     
   
       div.innerHTML = `
       
-      <label><i class="fas fa-align-justify"></i>&nbsp&nbsp${item.description}</label><br>
-      <i class="fas fa-calendar-plus"></i>&nbsp&nbsp${item.createDate}<br>
-      <i class="fas fa-calendar-times"></i>&nbsp&nbsp${item.deadline}<br>
-      <i class="fas fa-tag"></i>&nbsp&nbsp${item.tags}<br>
-      <i class="fas fa-info"></i>&nbsp&nbsp${item.status}<br>
-      <button class="edit" onclick="editTask()"><i class="fas fa-edit"></i></button>
+      <div><label><i class="fas fa-align-justify"></i>&nbsp&nbsp${item.description}</label></div>
+      <div><i class="fas fa-calendar-plus"></i>&nbsp&nbsp${item.createDate}</div>
+      <div><i class="fas fa-calendar-times"></i>&nbsp&nbsp${item.deadline}</div>
+      <div><i class="fas fa-tag"></i>&nbsp&nbsp${item.tags}</div>
+      <div><i class="fas fa-info"></i>&nbsp&nbsp${item.status}</div><br>
+      <button class="edit"><i class="fas fa-edit"></i></button>
       <button class='delete-button' type="submit"><i class='fas fa-trash'></i> </button>
-      <button class='check' ${checked} type="submit" onclick="toggle()"><i class='fas fa-check'></i></button>
+      <button class='check' ${checked} type="submit" ><i class='fas fa-check'></i></button>
       
       `;
       
@@ -70,6 +78,31 @@ function deleteTodo(id) {
 }
   
 getFromLocalStorage();
+
+
+
+function edit(id) {
+  
+  todos.forEach(function(item) {
+    if (item.id == id) {
+      modal.classList.add("mostrar")     
+      item.description = descriptione.value;
+    }  
+      
+  
+
+}
+);
+  
+  
+}
+
+
+function salvar(){
+  localStorage.setItem('todos', JSON.stringify(todos));
+  renderTodos(todos);
+}
+
   
 todoItemsList.addEventListener('click', function(event) {
     if (event.target.classList.contains('check')) {
@@ -79,5 +112,11 @@ todoItemsList.addEventListener('click', function(event) {
     if (event.target.classList.contains('delete-button')) {
       deleteTodo(event.target.parentElement.getAttribute('data-key'));
     }
+    if (event.target.classList.contains('edit')) {
+      edit(event.target.parentElement.getAttribute('data-key'));
+    }  
+    
 });   
+
+
 
